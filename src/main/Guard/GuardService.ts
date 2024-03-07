b380cd61c331deac4be5989d123d4e1da30e1fcc
Jaypee P. Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-03-07 11:02:04 +0800

    feat src/main/Guard/GuardService.ts: Add methods to GuardService interface

--- src/main/Guard/GuardService.ts
+++ src/main/Guard/GuardService.ts
@@ -1,5 +1,29 @@
+import { PaginatedContent } from "../../lib/repositories/PaginatedContent";
+import { PaginationOptions } from "../../lib/repositories/PaginationOptions";
+import { StudentEntrance } from "../Entrance/StudentEntrance";
+import { StudentExit } from "../Exits/StudentExit";
 import { Guard } from "./Guard";
 
 export interface GuardService{
-    registerRootAdmin(entity: Guard): Promise<Guard>;
+    registerGuard(entity: Guard): Promise<Guard>;
+    registerAdmin(entity: Guard): Promise<Guard>;
+    findAllGuards(paginationOptions: PaginationOptions): Promise<PaginatedContent<Guard>>;
+    findAllActiveGuards(paginationOptions: PaginationOptions): Promise<PaginatedContent<Guard>>;
+    findAllDisabledGuards(paginationOptions: PaginationOptions): Promise<PaginatedContent<Guard>>;
+    findGuardById(id: string): Promise<Guard>;
+    findGuardByEmail(email: string): Promise<Guard>;
+    countAllGuards(): Promise<number>;
+    countAllActiveGuards(): Promise<number>;
+    findAllAdmins(paginationOptions: PaginationOptions): Promise<PaginatedContent<Guard>>;
+    findAllNonAdmins(paginationOptions: PaginationOptions): Promise<PaginatedContent<Guard>>;
+    countEntrancesCheckedByGuardId(guardId: string): Promise<number>;
+    countExitsCheckedByGuardId(guardId: string): Promise<number>
+    findEntrancesCheckedByGuardId(guardId: string, paginationOptions: PaginationOptions): Promise<PaginatedContent<StudentEntrance>>;
+    findExitsCheckedByGuardId(guardId: string, paginationOptions: PaginationOptions): Promise<PaginatedContent<StudentExit>>;
+    existByEmail(email: string): Promise<boolean>;
+    findByEmail(email: string): Promise<Guard>;
+    findGuardByGender(gender: "male" | "female", paginationOptions: PaginationOptions): Promise<PaginatedContent<Guard>>;
+    findByGuardFirstname(firstname: string, paginationOptions: PaginationOptions): Promise<PaginatedContent<Guard>>;
+    findByGuardMiddlename(middlename: string, paginationOptions: PaginationOptions): Promise<PaginatedContent<Guard>>;
+    findByGuardLastname(lastname: string, paginationOptions: PaginationOptions): Promise<PaginatedContent<Guard>>;
 }
