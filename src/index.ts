d1abb03cbe3a38abf6018caf785aa264bc4e4c44
Jaypee P. Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-03-11 12:41:42 +0800

    feat: Add error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { guardRoute } from "./routes/GuardRoute";
+import { UnauthorizedError } from "./routes/UnauthorizedError";
+import { IncorrectEmailOrPasswordError } from "./main/Authentication/IncorrectEmailOrPasswordError";
 
 const app = new Hono();
 
@@ -9,7 +11,21 @@ const app = new Hono();
 
 app.route("/api/v1/guards", guardRoute);
 
-
+app.onError(async (err, c) => {
+    const { message } = err;
+    switch(true){
+        case err instanceof UnauthorizedError:
+            c.status(401);
+            return c.json({ message });
+        case err instanceof IncorrectEmailOrPasswordError:
+            c.status(401);
+            return c.json({ message });
+        default:
+            console.error(err);
+            c.status(500);
+            return c.json({ message: 'server error'});
+    }
+});
 
 
 console.log("server started on port 8080");
